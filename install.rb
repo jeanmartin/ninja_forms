a9472c7ae84e3c5b1aee2e2cc8e0692caba5ce0e
Jan Schwenzien <jan@schwenzien.info>
2008-12-09 19:38:25 +0100

    added resources

--- install.rb
+++ install.rb
@@ -1,1 +1,19 @@
 # Install hook code here
+
+puts "Copying views/forms..."
+src = File.join(File.dirname(__FILE__), 'templates', 'views', 'forms')
+dst = File.join(RAILS_ROOT, 'app', 'views')
+FileUtils.cp_r(src, dst)
+
+puts "Copying stylesheets/*..."
+src = File.join(File.dirname(__FILE__), 'templates', 'stylesheets', '.')
+dst = File.join(RAILS_ROOT, 'public', 'stylesheets')
+FileUtils.cp_r(src, dst)
+
+puts "Copying images/icons/*..."
+src = File.join(File.dirname(__FILE__), 'templates', 'images', 'icons, '.')
+dst = File.join(RAILS_ROOT, 'public', 'images', 'icons')
+FileUtils.cp_r(src, dst)
+
+puts "Installation complete!"
+

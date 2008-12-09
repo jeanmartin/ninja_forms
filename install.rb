7dcc08ff562f9c18dd5bdf8e48b24f5032918983
Jan Schwenzien <jan@schwenzien.info>
2008-12-09 20:12:09 +0100

    changed/added uninstall hook

--- install.rb
+++ install.rb
@@ -6,12 +6,12 @@ dst = File.join(RAILS_ROOT, 'app', 'views')
 FileUtils.cp_r(src, dst)
 
 puts "Copying stylesheets/*..."
-src = File.join(File.dirname(__FILE__), 'templates', 'stylesheets', '.')
+src = File.join(File.dirname(__FILE__), 'templates', 'stylesheets', 'ninja_formas.css')
 dst = File.join(RAILS_ROOT, 'public', 'stylesheets')
 FileUtils.cp_r(src, dst)
 
 puts "Copying images/icons/*..."
-src = File.join(File.dirname(__FILE__), 'templates', 'images', 'icons, '.')
+src = File.join(File.dirname(__FILE__), 'templates', 'images', 'icons, 'help.gif')
 dst = File.join(RAILS_ROOT, 'public', 'images', 'icons')
 FileUtils.cp_r(src, dst)
 
--- uninstall.rb
+++ uninstall.rb
@@ -1,1 +1,6 @@
 # Uninstall hook code here
+
+FileUrils.rm_r File.join(RAILS_ROOT, 'app', 'views', 'forms'), :force => true
+FileUrils.rm   File.join(RAILS_ROOT, 'public', 'stylesheets', 'ninja_forms.css'), :force => true
+FileUrils.rm   File.join(RAILS_ROOT, 'public', 'images', 'icons', 'help.gif'), :force => true
+
